a7d29111bf93e93845573df19ac0c27e3792f721
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2023-05-01 18:26:29 -0300

    Use listen data to create sockets + mild WebServer loop refactor

--- src/Response.cpp
+++ src/Response.cpp
@@ -3,71 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   Response.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lcouto <lcouto@student.42sp.org.br>        +#+  +:+       +#+        */
+/*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 23:27:53 by lcouto            #+#    #+#             */
-/*   Updated: 2023/05/02 22:19:45 by lcouto           ###   ########.fr       */
+/*   Updated: 2023/05/03 21:11:55 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "Response.hpp"
+#include "Response.hpp"
 
 Response::Response(void)
 {
     ResponseTools::initStatusCodes(this->_statusCodes);
     ResponseTools::initContentTypes(this->_contentTypes);
-    return ;
+    return;
 }
 
 Response::Response(Request request) : _request(request)
 {
     ResponseTools::initStatusCodes(this->_statusCodes);
     ResponseTools::initContentTypes(this->_contentTypes);
-    return ;
+    return;
 }
 
-Response::~Response(void)
-{
-    clear();
-    return ;
-}
+Response::~Response(void) { clear(); }
 
-Response::Response(Response const &other)
-{
-    *this = other;
-	return ;
-}
+Response::Response(Response const &other) { *this = other; }
 
 Response &Response::operator=(Response const &other)
 {
     if (this != &other) {
-        this->_request = other._request;
+        this->_request        = other._request;
         this->_responseString = other._responseString;
-        this->_statusLine = other._statusLine;
-        this->_headers = other._headers;
-        this->_body = other._body;
-        this->_status = other._status;
-        this->_type = other._type;
-        this->_serverData = other._serverData;
+        this->_statusLine     = other._statusLine;
+        this->_headers        = other._headers;
+        this->_body           = other._body;
+        this->_status         = other._status;
+        this->_type           = other._type;
+        this->_serverData     = other._serverData;
     }
     return *this;
 }
 
-std::string Response::getResponseString(void)
-{
-    return this->_responseString;
-}
+std::string Response::getResponseString(void) { return this->_responseString; }
 
-void  Response::setServerData(Server *serverData)
-{
-    this->_serverData = serverData;
-}
+void Response::setServerData(Server *serverData) { this->_serverData = serverData; }
 
-void Response::setRequest(Request request)
-{
-    this->_request = request;
-    return ;
-}
+void Response::setRequest(Request request) { this->_request = request; }
 
 void Response::assembleResponseString(void)
 {
@@ -76,8 +58,9 @@ void Response::assembleResponseString(void)
     assembleBody();
     assembleHeaders();
     assembleStatusLine();
-    
-    for (std::map<std::string, std::string>::iterator it = this->_headers.begin(); it != this->_headers.end(); ++it) {
+
+    std::map<std::string, std::string>::iterator it;
+    for (it = this->_headers.begin(); it != this->_headers.end(); ++it) {
         headersString = headersString + it->first + ": " + it->second + CRLF;
     }
 
@@ -88,10 +71,11 @@ void Response::assembleResponseString(void)
 
 void Response::assembleStatusLine()
 {
-    std::string statusCode, statusMessage, protocol;
+    std::string                                  statusCode, statusMessage, protocol;
     std::map<std::string, std::string>::iterator it = this->_statusCodes.find(this->_status);
-    protocol = "HTTP/1.1";
-    statusCode = it->first;
+
+    protocol      = "HTTP/1.1";
+    statusCode    = it->first;
     statusMessage = it->second;
 
     this->_statusLine = protocol + SP + statusCode + SP + statusMessage;
@@ -107,7 +91,7 @@ void Response::assembleHeaders()
         this->_headers.insert(std::make_pair("Content-Length", "0"));
     }
 
-    std::string contentType;
+    std::string                                  contentType;
     std::map<std::string, std::string>::iterator it = this->_contentTypes.find(this->_type);
     if (it->second == "") {
         contentType = "application/octet-stream";
@@ -120,50 +104,29 @@ void Response::assembleHeaders()
 void Response::assembleBody()
 {
     std::string resourcePath, requestURI, root, resource;
-    std::vector<std::string> indexes;
 
-    std::vector<Location> locations = this->_serverData->getLocations();
-    requestURI = this->_request.getRequestURI();
-    if (!locations.empty()) {
-        for (size_t i = 0; i < locations.size(); i++) {
+    requestURI   = this->_request.getRequestURI();
+    root         = this->_serverData->getValue("root")[0];
+    resourcePath = ResponseTools::assemblePath(root, requestURI);
 
-            std::string locationPath = locations[i].getPath();
-            indexes = locations[i].getValue("index");
-            root = locations[i].getValue("root").empty() ? this->_serverData->getValue("root")[0] : locations[i].getValue("root")[0];
-            resourcePath = ResponseTools::assemblePath(root, requestURI);
-            
-            indexes = locations[i].getValue("index");
-            if (!indexes.empty()) {
-                for (size_t i = 0; i < indexes.size(); i++) {
-                    resource = ResponseTools::endsWith(resourcePath, indexes[i]) ? resourcePath : resourcePath + indexes[i];
-                    if (ResponseTools::fileExists(resource)) {
-                        this->_status = "200";
-                        break ;
-                    }
-                }
-            } else {
-                resource = resourcePath;
-                if (ResponseTools::fileExists(resource) && resource.find(locationPath) != std::string::npos) {
-                    this->_status = "200";
-                }
-            }
-            if (this->_status == "200")
-                break ;
+    std::vector<std::string> indexes
+        = this->_serverData->getValue("index"); // not looking into locations yet.
+    for (size_t i = 0; i < indexes.size(); i++) {
+        resource = resourcePath + indexes[i];
+        if (ResponseTools::fileExists(resource)) {
+            this->_status = "200";
+            break;
         }
-    } 
-    if (this->_status != "200") {
-        indexes = this->_serverData->getValue("index");
-        root = this->_serverData->getValue("root")[0];
-        resourcePath = ResponseTools::assemblePath(root, requestURI);
-
         this->_status = "404";
-        
+
         if (!indexes.empty()) {
             for (size_t i = 0; i < indexes.size(); i++) {
-                resource = ResponseTools::endsWith(resourcePath, indexes[i]) ? resourcePath : resourcePath + indexes[i];
+                resource = ResponseTools::endsWith(resourcePath, indexes[i])
+                    ? resourcePath
+                    : resourcePath + indexes[i];
                 if (ResponseTools::fileExists(resource)) {
                     this->_status = "200";
-                    break ;
+                    break;
                 }
             }
         } else {
@@ -177,19 +140,20 @@ void Response::assembleBody()
     if (ResponseTools::isDirectory(resource))
         this->_status = "404";
 
-    if (this->_status == "404")
-    {
+    if (this->_status == "404") {
         this->_type = "html";
-        resource = "./examples/404.html";
+        resource    = "./examples/404.html";
         std::ifstream error404(resource.c_str());
-        std::string body((std::istreambuf_iterator<char>(error404)), std::istreambuf_iterator<char>());
+        std::string   body((std::istreambuf_iterator<char>(error404)),
+                         std::istreambuf_iterator<char>());
         this->_body = body;
-        return ;
+        return;
     }
-    
+
     this->_type = ResponseTools::getFileExtension(resource);
     std::ifstream resourceContent(resource.c_str());
-    std::string body((std::istreambuf_iterator<char>(resourceContent)), std::istreambuf_iterator<char>());
+    std::string   body((std::istreambuf_iterator<char>(resourceContent)),
+                     std::istreambuf_iterator<char>());
 
     if (body.length() == 0)
         this->_status = "204";
