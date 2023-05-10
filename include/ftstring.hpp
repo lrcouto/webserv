d5be4e1238cc11a205a3acec764c2587a7f539e1
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2023-05-10 18:01:02 -0300

    Normalize request header fields to lowercase

--- include/ftstring.hpp
+++ include/ftstring.hpp
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 18:36:10 by maolivei          #+#    #+#             */
-/*   Updated: 2023/04/24 12:10:32 by maolivei         ###   ########.fr       */
+/*   Updated: 2023/05/10 16:42:25 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ std::string reduce(std::string const &str, std::string const &set, std::string c
 std::vector<std::string> split(std::string const &str, char delimiter);
 bool                     is_positive_integer(std::string const &str);
 int                      strtoi(std::string const &str);
+bool                     striequals(std::string const &s1, std::string const &s2);
 
 } /* ftstring */
 
